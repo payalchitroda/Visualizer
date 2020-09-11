83654821db8ca9859b3b62ee86830e3d0053a3bf
payal chitroda <payalk.chitroda@gmail.com>
2020-09-11 19:08:54 +0530

    graphinput

--- src/pages/algo.js
+++ src/pages/algo.js
@@ -2,16 +2,43 @@ import React from 'react';
 import Node from '../components/node';
 function Algo() {
 
-    return(
+    function graphinput(node, edges) {
+        var n = parseInt(node);
+        console.log(node, edges);
+        var e = [];
+        var k=0;
+        for (var i = 0; i < n; i++) {
+            var data = [];
+            for (var j = 0; j < 3; j++) {
+                data.push(edges.charAt(k));
+                k+=2;
+            }
+            e.push(data);
+        }
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < 3; j++) {
+                console.log(e[i][j]);
+            } 
+        }
+
+
+
+    }
+    let n;
+    let edges;
+    return (
         <div>
-       
-        <div style={{ backgroundColor: "LightGray" ,height:"630px" }}>
-        <h1>Kruskal Algorithm</h1>
-                input<input type="text"/>
-                <input type="submit" value="submit"/>
+
+            <div style={{ backgroundColor: "LightGray", height: "630px" }}>
+                <h1>Kruskal Algorithm</h1>
+                Enter number of nodes<input type="text" ref={node => { n = node; }} />
+                Edges<input type="text" ref={node => { edges = node; }} />
+                <button onClick={() => { graphinput(n.value, edges.value); n.value = ''; edges.value = ''; }}>
+                    Submit
+                </button>
                 <Node></Node>
             </div>
-           
+
         </div>
     );
 
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Algo() {
+    
+    return(
+        <div>
+       
+           
+        </div>
+    );
+
+}
+
+export default Algo;
