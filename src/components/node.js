d3530cfa244932cdc8f6923d2329d3e7dd82fdc1
payal chitroda <payalk.chitroda@gmail.com>
2020-09-12 01:31:02 +0530

    dynamic nodes

--- src/components/node.js
+++ src/components/node.js
@@ -2,14 +2,37 @@ import React from 'react';
 import Konva from 'konva';
 //import { render } from 'react-dom';
 import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
-function Node() {
+function Node(props) {
+  var a=props.numberofnodes
+  var b=props.data;
+  //var a=10;
+  //var b=[[1,2,3],[2,3,4],[4,1,6]];
+  var cx=0,cy1=100,w=300,cy2=300,cy;
+  var flag=true;
+  var graph = []
+  for(var i=0;i<a;i++)
+  {
+    if(flag)
+    {
+      cx=cx+w;
+      cy=cy1;
+    }
+    else
+    {
+      cy=cy2;
+    }
+    flag=!flag;
+    graph.push(<Circle id ={i+1} x={cx} y={cy} radius={50} fill="DarkGoldenRod"/>);
+    graph.push(<Text text={i+1} x={cx} y={cy} fontSize={20}/>);
     
+  }
+  
     return(
 
-        <Stage width={800} height={800}>
-        <Layer>
-   
-          <Circle id ="1" x={200} y={100} radius={50} fill="DarkGoldenRod"/>
+        <Stage width={1600} height={800}>
+        <Layer id="layer">
+          {graph}
+         {/* <Circle id ="1" x={200} y={100} radius={50} fill="DarkGoldenRod"/>
           <Text text="1" x={200} y={100} fontSize={20}/>
           <Line points={[250, 100, 450,100]} stroke='black' strokeWidth={3}/>
           <Circle id ="2" x={500} y={100} radius={50} fill="DarkGoldenRod" />
@@ -18,7 +41,7 @@ function Node() {
           <Circle id ="3" x={300} y={300} radius={50} fill="DarkGoldenRod" />
           <Text text="3" x={300} y={300} fontSize={20}/>
           <Line points={[500, 150, 300,250]} stroke='black' strokeWidth={3}/>
-         
+    */}
         </Layer>
       </Stage>
     );
--- src/pages/algo.js
+++ src/pages/algo.js
@@ -1,47 +1,55 @@
 import React from 'react';
 import Node from '../components/node';
-function Algo() {
-
-    function graphinput(node, edges) {
+class Algo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberofnodes:{},
+            data:{}
+        };
+    }
+    
+    graphinput(node, edges) {
         var n = parseInt(node);
-        console.log(node, edges);
+       // console.log(node, edges);
         var e = [];
-        var k=0;
+        var k = 0;
         for (var i = 0; i < n; i++) {
             var data = [];
             for (var j = 0; j < 3; j++) {
                 data.push(edges.charAt(k));
-                k+=2;
+                k += 2;
             }
             e.push(data);
         }
         for (var i = 0; i < n; i++) {
             for (var j = 0; j < 3; j++) {
                 console.log(e[i][j]);
-            } 
+            }
         }
-
-
-
+        this.setState({data:e,numberofnodes:n});
     }
-    let n;
-    let edges;
-    return (
-        <div>
 
-            <div style={{ backgroundColor: "LightGray", height: "630px" }}>
-                <h1>Kruskal Algorithm</h1>
+    render() {
+        let n;
+        let edges;
+        return (
+            <div>
+
+                <div style={{ backgroundColor: "LightGray", height: "630px" }}>
+                    <h1>Kruskal Algorithm</h1>
                 Enter number of nodes<input type="text" ref={node => { n = node; }} />
                 Edges<input type="text" ref={node => { edges = node; }} />
-                <button onClick={() => { graphinput(n.value, edges.value); n.value = ''; edges.value = ''; }}>
-                    Submit
+                    <button onClick={() => { this.graphinput(n.value, edges.value); n.value = ''; edges.value = ''; }}>
+                        Submit
                 </button>
-                <Node></Node>
+                    <Node data={this.state.data} numberofnodes = {this.state.numberofnodes}/>
+                </div>
+
             </div>
-
-        </div>
-    );
-
+        );
+    }
 }
+
 
 export default Algo;
