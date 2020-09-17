fd94b10d43a4d917dd4f8c662a64861e48ada73c
payal chitroda <payalk.chitroda@gmail.com>
2020-09-17 20:00:23 +0530

    algo2

--- src/components/kruskal.js
+++ src/components/kruskal.js
@@ -5,8 +5,8 @@ class Kruskal extends React.Component {
     n=0;
     parent =[];
     graph =[]
-
     
+
     union(x, y) {
        
         var px = this.find(x);
@@ -28,33 +28,40 @@ class Kruskal extends React.Component {
     }
 
     minimumCost(N, connections) {
-        this.parent = Array(N+1).fill(0);
-        console.log("parent"+N);
-        // for(var i=0;i<this.parent.length;i++)
-        // {
-        //     console.log(this.parent[i]);
-        // }
+        console.log("node____"+N);
         for (var i = 0; i <= N; i++) {
             this.parent[i] = i;
         }
-        connections.sort(function (a, b) { return (a[2] - b[2]) });
 
+       // console.log("node____"+props.nodes);
+        connections.sort(function (a, b) { return (a[2] - b[2]) });
+        //this.graph.push(<Circle id={1} x={200} y={100} radius={50} fill="blue" />);
         for (var i = 0; i < connections.length; i++) {
 
             var x = connections[i][0], y = connections[i][1];
-
-
-             console.log(x + " " + y);
+           
+            
+            
+            // console.log(x + " " + y);
             if (this.find(x) != this.find(y)) {
-                var index1 = (x - 1) * 2;
-                var index2 = (y - 1) * 2;
-                var a = this.props.g[index1];
-                var c = this.props.g[index2];
-                this.state.graph.push(<Circle id={a.props.id} x={a.props.cx + 50} y={a.props.cy + 50} radius={50} fill="blue" />);
-                console.log(x + " " + y);
+                
+               
+              
+                var index1 = (x-1) * 2;
+                var index2 = (y-1)  * 2;
+                
+                var a = this.graph[index1];
+                var c = this.graph[index2];
+               // console.log("index"+index1+ " " + index2);
+               // console.log("nodes"+a.props.id+ " " +a.props.x+ " " + a.props.y);
+
+            // this.graph.push(<Circle id={a.props.id} x={a.props.cx} y={a.props.cy} radius={100} fill="blue" />);
+               // console.log(a.props.fill);
+                //console.log(a.props.cx + " " + a.props.cy);
                 console.log("hello");
                 //document.write(x+" "+y);
                 this.union(x, y);
+
             }
         }
 
@@ -66,9 +73,10 @@ class Kruskal extends React.Component {
 
         return (
             <div>
-                <div onLoad={this.minimumCost(this.props.nodes, this.props.connections)}>
+                <div onLoad={this.minimumCost(4, [[1, 2, 3], [2, 3, 4], [1, 4, 2]])}>
+                {/* <div onLoad={this.minimumCost(this.props.nodes, this.props.connections)}> */}
                     <Stage width={1600} height={800}>
-                        <Layer id="layer">
+                        <Layer id="layer"  >
                             {this.graph}
                         </Layer>
                     </Stage>
--- src/components/node.js
+++ src/components/node.js
@@ -1,90 +1,156 @@
-import React from 'react';
+// import React from 'react';
+// import Konva from 'konva';
+// import Kruskal from './kruskal.js';
+// import { render } from 'react-dom';
+// import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+// import { Button } from 'react-bootstrap';
+
+// class Node extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       a: props.numberofnodes,
+//       b: props.data,
+//       graph: [],
+//       lines: []
+//     }
+//     this.mount = this.mount.bind(this);
+//   }
+
+
+//   mount() {
+
+
+//     var cx = 0, cy1 = 100, w = 300, cy2 = 300, cy;
+//     var flag = true;
+
+//     console.log("a" + this.state.a);
+//     var g = [];
+//     for (var i = 0; i < this.state.a; i++) {
+//       if (flag) {
+//         cx = cx + w;
+//         cy = cy1;
+//       }
+//       else {
+//         cy = cy2;
+//       }
+//       flag = !flag;
+//       this.g.push(<Circle id={i + 1} x={cx} y={cy} radius={50} fill="DarkGoldenRod" />);
+//       this.g.push(<Text text={i + 1} x={cx} y={cy} fontSize={20} />);
+
+//     }
+
+
+//     var l = [];
+//     for (var i = 1; i <= this.state.b.length; i++) {
+//       var index1 = (this.state.b[i - 1][0] - 1) * 2;
+//       var index2 = (this.state.b[i - 1][1] - 1) * 2;
+//       var weight = this.state.b[i - 1][2];
+//       var a = this.state.graph[index1];
+//       var c = this.state.graph[index2];
+//       this.l.push(<Line points={[a.props.x, a.props.y, c.props.x, c.props.y]} stroke='black' strokeWidth={3} />)
+//       var tx = ((a.props.x + c.props.x) / 2);
+//       var ty = ((a.props.y + c.props.y) / 2);
+//       this.l.push(<Text text={weight} x={tx} y={ty} fontSize={20} />)
+//     }
+
+//     this.setState({ graph: g, lines: l });
+//   }
+//   componentDidMount() {
+//     this.mount();
+//   }
+
+
+//   render() {
+//     return (
+
+//       <div >
+//         <button onClick={this.kruskalrender}>step</button>
+
+//         {/* <Kruskal nodes={this.a} connections={this.b} g={this.state.graph} l={this.state.lines} /> */}
+
+
+//       </div>
+//     );
+
+//   }
+// }
+
+
+// export default Node;
+
+import React, { useState } from 'react';
 import Konva from 'konva';
-//import {Kruskal} from '../pages/kruskal.js';
-import Kruskal from './kruskal.js';
-import { render } from 'react-dom';
+ import Kruskal from './kruskal.js';
+
+//import { render } from 'react-dom';
 import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
-import { Button } from 'react-bootstrap';
 
-class Node extends React.Component {
-  a=0;
-  b=0;
-  kruskal = [];
-  graph = [];
-  lines = [];
-  componentDidUpdate() {
-    var cx = 0, cy1 = 100, w = 300, cy2 = 300, cy;
-    var flag = true;
-    this.a = this.props.numberofnodes;
-    this.b = this.props.data;
-    for (var i = 0; i < this.a; i++) {
-      if (flag) {
-        cx = cx + w;
-        cy = cy1;
-      }
-      else {
-        cy = cy2;
-      }
-      flag = !flag;
-      this.graph.push(<Circle id={i + 1} x={cx} y={cy} radius={50} fill="DarkGoldenRod" />);
-      this.graph.push(<Text text={i + 1} x={cx} y={cy} fontSize={20} />);
-
-    }
-    console.log("length" + this.b.length)
-
-    for (var i = 1; i <= this.b.length; i++) {
-      var index1 = (this.b[i - 1][0] - 1) * 2;
-      var index2 = (this.b[i - 1][1] - 1) * 2;
-      var weight = this.b[i - 1][2];
-      var a = this.graph[index1];
-      var c = this.graph[index2];
-      //console.log(index1+" "+index2);
-      this.lines.push(<Line points={[a.props.x, a.props.y, c.props.x, c.props.y]} stroke='black' strokeWidth={3} />)
-      var tx = ((a.props.x + c.props.x) / 2);
-      var ty = ((a.props.y + c.props.y) / 2);
-      this.lines.push(<Text text={weight} x={tx} y={ty} fontSize={20} />)
-     // console.log(this.graph);
-     // console.log(a.props.x +" "+a.props.y+" "+c.props.x+" "+c.props.y);
-    }
-  }
+function Node(props) {
+  // const [a, setA] = useState(props.numberofnodes);
+  // const [b, setB] = useState(props.data);
+  // const [graph, setGraph] = useState([]);
+  // const [lines, setLines] = useState([]);
+  var a=props.numberofnodes
+  var b=props.data;
+  var g=[]
+ var l=[]
+  var cx=0,cy1=100,w=300,cy2=300,cy;
+  var flag=true;
  
-  kruskalrender() {
-
-    this.kruskal.push(<Kruskal nodes={this.a} connections={this.b} g={this.graph} l={this.lines} />);
+  for(var i=0;i<a;i++)
+  {
+    if(flag)
+    {
+      cx=cx+w;
+      cy=cy1;
+    }
+    else
+    {
+      cy=cy2;
+    }
+    flag=!flag;
+    g.push(<Circle id ={i+1} x={cx} y={cy} radius={50} fill="DarkGoldenRod"/>);
+    g.push(<Text text={i+1} x={cx} y={cy} fontSize={20}/>);
+    
   }
 
 
+  for(var i=1;i<=b.length;i++)
+  {
+    var index1= (b[i-1][0]-1)*2;
+    var index2= (b[i-1][1]-1)*2;
+    var weight =b[i-1][2];
+    var d= g[index1];
+    var c=g[index2];
+    console.log(index1+" "+index2);
+    l.push(<Line points={[d.props.x, d.props.y, c.props.x,c.props.y]} stroke='black' strokeWidth={3}/>)
+    var tx=((d.props.x+c.props.x)/2);
+    var ty=((d.props.y+c.props.y)/2);
+  l.push( <Text text={weight} x={tx} y={ty} fontSize={20}/>)
 
+    console.log(d.props.x +" "+d.props.y+" "+c.props.x+" "+c.props.y);
+     
+  }
+  // setGraph(g);
+  // setLines(l);
+  
+    return(
 
-render() {
-  return (
-
-    <div>
-<button onClick={this.kruskalrender}>play</button>
-      <Stage width={1600} height={800}>
+        <Stage width={1600} height={800}>
         <Layer id="layer">
-          {this.graph}
-          {this.lines}
-         
-          {/* <Circle id ="1" x={200} y={100} radius={50} fill="DarkGoldenRod"/>
-          <Text text="1" x={200} y={100} fontSize={20}/>
-          <Line points={[250, 100, 450,100]} stroke='black' strokeWidth={3}/>
-          <Circle id ="2" x={500} y={100} radius={50} fill="DarkGoldenRod" />
-          <Text text="2" x={500} y={100} fontSize={20}/>
-          <Line points={[200, 150, 300,250]} stroke='black' strokeWidth={3}/>
-          <Circle id ="3" x={300} y={300} radius={50} fill="DarkGoldenRod" />
-          <Text text="3" x={300} y={300} fontSize={20}/>
-          <Line points={[500, 150, 300,250]} stroke='black' strokeWidth={3}/>
-    */}
-
+          {/* {graph}
+          {lines} */}
+          {g}
+          {l}
+        {/* <Kruskal nodes={a} connections={b} g={graph} l={lines} />
+         */}
         </Layer>
       </Stage>
-      {this.a}
-      {/* <Kruskal nodes={this.a} connections={this.b} /> */}
-    </div>
-  );
+    );
 
-}
 }
 
 
